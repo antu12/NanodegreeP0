f1f31d114ac1024aabf6a6eec4ac7695f563af9f
Arshad Hossain <arshad.antu@gmail.com>
2016-09-11 22:58:00 +0600

    all button has a different toast methods

--- app/src/main/java/com/example/arshad/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/arshad/myappportfolio/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,34 +14,82 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = (Button) findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This will open popular movies app!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        Button b2 = (Button) findViewById(R.id.b2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This will open stock hawk app!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        Button b3 = (Button) findViewById(R.id.b3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This will open build it bigger app!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        Button b4 = (Button) findViewById(R.id.b4);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This will open make your app material!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        Button b5 = (Button) findViewById(R.id.b5);
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This will open go ubiquious app!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+        Button b6 = (Button) findViewById(R.id.b6);
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "This will open my capstone app!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
 
-    public void showToast(View view){
-        Context context = getApplicationContext();
-        CharSequence text="";
-        switch (view.getId()){ // got the idea of switch from stackoverflow 
-            case (R.id.b1):
-                text="This button will launch my popular movies app!";
-                break;
-            case (R.id.b2):
-                text="This button will launch my stock hawk app!";
-                break;
-            case (R.id.b3):
-                text="This button will launch my build it bigger app!";
-                break;
-            case (R.id.b4):
-                text="This button will launch my make your app material!";
-                break;
-            case (R.id.b5):
-                text="This button will launch my go ubiquitous app!";
-                break;
-            case (R.id.b6):
-                text="This button will launch my capstone app!";
-                break;
-        }
-        int duration = Toast.LENGTH_SHORT;
 
-        Toast.makeText(context, text, duration).show();
-    }
 }
